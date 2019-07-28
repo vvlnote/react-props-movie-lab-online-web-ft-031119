c24f0b0cff311f6400599e0fbcc5e0b3cbc8325e
Joy Lin <vvlnote@hotmail.com>
2019-07-28 17:08:31 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,10 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
+    // map over your movieData array and return the correct
+    movieData.map((data) => {
+      
+    })
   }
 
   render() {
