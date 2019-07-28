b5f1da144852af309673632c7f4c38b07486c172
Joy Lin <vvlnote@hotmail.com>
2019-07-28 19:16:17 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -41,3 +41,9 @@ export default class MovieCard extends Component {
 }
 
 // Don't forget your default props!
+MovieCard.defaultProps ={
+  title: "Unknown";
+  IMDBRating: null;
+  genres:"No Genre(s) Found";
+  poster: "default";
+}
